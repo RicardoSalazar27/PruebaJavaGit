8323639e4fd56a5062283ce54ea3fb07b1896c4f
EDGAR RICARDO SESENES SALAZAR <edgar_ss2012@hotmail.com>
2022-05-23 20:13:35 -0500

    Hola BUENAS TARDES

--- src/pruebag/PruebaG.java
+++ src/pruebag/PruebaG.java
@@ -16,6 +16,7 @@ public class PruebaG {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("HOLA RICARDOOO");
     }
     
 }
